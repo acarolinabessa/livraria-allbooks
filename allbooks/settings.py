0819017ec7ebe1059b53bf640a6b5edab3edd42a
Ana Carolina <anabessa@aluno.uema.br>
2022-07-19 15:32:59 -0300

    crispy forms

--- allbooks/settings.py
+++ allbooks/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cpf_field',
+    'crispy_forms',
     'accounts',
     'autores',
     'compras',
